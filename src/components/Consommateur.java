01a4984f8f25d68dc403fb7ed723e62f05568b64
Nassim <nassim.et@hotmail.fr>
2019-01-27 02:29:13 +0100

    add implements ReceptionI PublicationI

--- src/components/Consommateur.java
+++ src/components/Consommateur.java
@@ -1,12 +1,25 @@
 package components;
 
 import fr.sorbonne_u.components.AbstractComponent;
+import interfaces.ReceptionI;
 
-public class Consommateur extends AbstractComponent {
+public class Consommateur extends AbstractComponent implements ReceptionI {
 
 	public Consommateur(String reflectionInboundPortURI, int nbThreads, int nbSchedulableThreads) {
 		super(reflectionInboundPortURI, nbThreads, nbSchedulableThreads);
 
+	}
+
+	@Override
+	public void recevoirMessage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void recevoirNMessage() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/components/Producteur.java
+++ src/components/Producteur.java
@@ -1,11 +1,24 @@
 package components;
 
 import fr.sorbonne_u.components.AbstractComponent;
+import interfaces.PublicationI;
 
-public class Producteur extends AbstractComponent {
+public class Producteur extends AbstractComponent implements PublicationI {
 
 	public Producteur(String reflectionInboundPortURI, int nbThreads, int nbSchedulableThreads) {
 		super(reflectionInboundPortURI, nbThreads, nbSchedulableThreads);
+	}
+
+	@Override
+	public void publierMessage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void publierNMessage() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
