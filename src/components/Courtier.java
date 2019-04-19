1da66f4f7c002509a6fe91f65b3d88bc53c3ad91
Nassim <nassim.et@hotmail.fr>
2019-04-19 13:51:07 +0200

    Sur la bonne voie

--- src/components/Courtier.java
+++ src/components/Courtier.java
@@ -32,8 +32,8 @@ public class Courtier extends AbstractComponent {
 	public Courtier() throws Exception {
 		super(1, 0);
 
-		createNewExecutorService("publication", 100, false);
-		createNewExecutorService("envoi", 100, false);
+		createNewExecutorService("publication", 5, false);
+		createNewExecutorService("envoi", 5, false);
 
 		String publicationPortURI = java.util.UUID.randomUUID().toString();
 		publicationPort = new PublicationInboundPort(publicationPortURI, this);
